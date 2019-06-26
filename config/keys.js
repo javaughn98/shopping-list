99de3a440bed634651bcb3bda0738eb1bb020eb4
Javaughn Burke <javaughnburke@Javaughns-MacBook-Air.local>
2019-06-26 17:46:15 -0400

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://Javaughn:17romario,@shopping-list-otibs.mongodb.net/test?retryWrites=true&w=majority'
+};
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const items = require('./routes/api/items');
+const app = express();
+
+// bodyparser middleware
+
+app.use(bodyParser.json());
+
+//Database Config
+
+const db = require('./config/keys').mongoURI;
+
+// connect to mongodb
+mongoose.connect(db, { useNewUrlParser: true }).then(() => 
+    console.log('MongoDB is Connected....')
+).catch(err => console.log(err));
+
+// Use routes
+app.use('/api/items', items);
+
+//Serve static assets if in production
+
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+  
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`server started on port ${port}`);
+});
